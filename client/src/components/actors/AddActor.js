e7e3eecef7531aed0ceed29aae1bee15d39ea5c8
anamarijaeres <ae50637@fer.hr>
2023-03-03 14:34:58 +0100

    First commit

---
+++ client/src/components/actors/AddActor.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import axios from "axios";
+import { Button, Checkbox, Form,Input,Message } from 'semantic-ui-react'
+
+let endpoint = "http://localhost:8080";
+
+class AddActor extends React.Component{
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            firstName: "",
+            lastName: "",
+        };
+        this.onSubmit=this.onSubmit.bind(this);
+    }
+    
+    
+        onChange =  (event) => {
+            this.setState({
+                [event.target.name]: event.target.value,
+            });
+            this.onSubmit.bind(this);
+        };
+        onSubmit =() => {
+            
+            if(this.state.firstName.length==0 || this.state.lastName.length==0){
+                alert("You cannot add an actor with blank first/last name!")
+            }else{
+
+           
+            console.log(this.state.firstName)
+            console.log(this.state.lastName)
+            
+            axios
+            .post(
+                    endpoint + "/actors",
+                    {
+                        FirstName:this.state.firstName,
+                        LastName:this.state.lastName
+                    },
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                        
+                        },
+                    }
+                )
+                .then((res) => {
+                    this.setState({
+                        firstName: "",
+                        lastName:""
+                    });
+                    console.log(res);
+                    console.log(res.status);
+                    alert("You've added new actor!")
+                }).catch((error) =>{
+                    alert("You cannot ")
+                    console.log(error)});
+                }
+            
+        };
+
+        render() {
+            return(
+            <div>
+                <h3>Add a new Actor</h3>
+                <Form onSubmit={this.onSubmit}>
+                    <Form.Field>
+                        <label>First Name</label>
+                        <Input error placeholder='First Name' name="firstName" type="text" value={this.state.firstName} onChange={this.onChange}/>
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Last Name</label>
+                        <input placeholder='Last Name' name="lastName" type="text" value={this.state.lastName} onChange={this.onChange}/>
+                    </Form.Field>
+                    <Button type='submit'>Submit</Button>
+
+                </Form>
+            </div>
+            )
+        };
+    
+}
+
+export default AddActor
